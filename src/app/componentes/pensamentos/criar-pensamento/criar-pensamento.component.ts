ed5cd2bb432160855d23cf05ffd25efd39cc9d1d
Julio Henrique Diaz Viana dos santos <juliohenriquediazvianadossantos@MacBook-Air-de-Julio.local>
2024-06-08 16:10:26 -0300

    Event Bind e diretiva ngModel (Two-way - data binding)

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -11,12 +11,20 @@ export class CriarPensamentoComponent implements OnInit {
     id: "1",
     conteudo: "Aprendendo Angular",
     autoria: "Dev",
-    modelo: ""
+    modelo: "modelo1"
   };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  criaPensamento(): void {
+    alert("Sucesso ao criar pensamento");
+  }
+
+  cancelaPensamento(): void {
+    alert("Sucesso ao cancelar pensamento");
   }
 
 }
