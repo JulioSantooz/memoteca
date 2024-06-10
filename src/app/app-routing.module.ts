7f8125141512060b21b1ef6f3222b1c4b2da0cc0
Julio Henrique Diaz Viana dos santos <juliohenriquediazvianadossantos@MacBook-Air-de-Julio.local>
2024-06-10 17:34:18 -0300

    trabalhando com as rotas das paginas 'criarPensamento' e 'listarPensamentos'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mural-de-pensamentos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'mural-de-pensamentos',
+    component: ListarPensamentoComponent
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
