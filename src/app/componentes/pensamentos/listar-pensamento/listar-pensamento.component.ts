ddeff9850b9b947b49291e8f64a356b3fa89c015
Julio Henrique Diaz Viana dos santos <juliohenriquediazvianadossantos@MacBook-Air-de-Julio.local>
2024-06-10 21:58:50 -0300

    trabalhando comcdiretivas 'ngIf', 'ngFor' e 'ngClass'

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Teste ...',
+      autoria: 'Julio Santos',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Teste2 ...',
+      autoria: 'Julio Santos',
+      modelo: 'modelo1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
